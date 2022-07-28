45f1d3d9f4b550d1c66ee1ba13ceddcf33bd5f52
rootware <ominbruce@outlook.com>
2022-07-28 20:50:53 +0800

    changed import path

--- tests/test_cregex_improved.py
+++ tests/test_cregex_improved.py
@@ -1,6 +1,6 @@
 import pytest
 import re
-import commonregex_improved as cregex
+import commonregex.commonregex_improved as cregex
 
 # def test_cregex_find_all():
 #   text = "John, please get that article on www.linkedin.com to me by 5:00PM on Jan 9th 2012. 4:00 would be ideal, actually. If you have any questions, You can reach me at (519)-236-2723x341 or get in touch with my associate at harold.smith@gmail.com"
